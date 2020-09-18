56637c57d736f6d1e5ab39549c8c7587b17eb7ba
tysend <tysen.dauer@gmail.com>
2020-09-18 10:40:04 -0400

    example_damp.m update and plots

--- example_damp.m
+++ example_damp.m
@@ -1,3 +1,37 @@
+% %% Paths
+% % Path 1: use wav_risefall() with 'lin' and then apply mag2db.
+% % Path 2: apply mag2db inside of wav_risefall().
+% 
+% %% Path 1
+% %% Create Complex Sound with Envelope
+% Sf =    44100;         % sampling frequency
+% dur =   10;            % duration
+% fR =    [900 1800];    % frequency range
+% N =     150;           % # of frequency components
+% Mf =    4;             % modulation frequency (AM) in Hz
+% type =  'damped';      % type of modulation: 'ramped' or 'damped'
+% param = 1.15;          % steepness of the modulation (1.05, 1.49, 2, etc.)
+% 
+% [y t] = generate_complex_envel(Sf,dur,fR,N,Mf,type,param);
+% % Output:
+% % y // amplitude-modulated vector
+% % t // corresponding time vector
+% 
+% %% rise/fall
+% [y w] = wav_risefall(y,[5 5],Sf,'lin'); % hijacked version of this function
+% % [y w] = wav_risefall(y,[0.01 0.01], Sf, 'lin'); % is this an appropriate amount of rise/fall time?
+% ty = mag2db(y);
+% 
+% %% Plot
+% plot(t, ty)
+
+%% Listen
+% sound(ty, Sf)
+
+%% Export audio file
+% audiowrite('example_damp.wav',y,Sf)
+
+%% Path 2
 %% Create Complex Sound with Envelope
 Sf =    44100;         % sampling frequency
 dur =   10;            % duration
@@ -16,12 +50,9 @@ param = 1.15;          % steepness of the modulation (1.05, 1.49, 2, etc.)
 [y w] = wav_risefall(y,[5 5],Sf,'db'); % hijacked version of this function
 % [y w] = wav_risefall(y,[0.01 0.01], Sf, 'lin'); % is this an appropriate amount of rise/fall time?
 
+
 %% Plot
 plot(t, y, t, w)
-% plot(t,y)
 
 %% Listen
-% sound(y, Sf)
-
-%% Export audio file
-% audiowrite('example_damp.wav',y,Sf)
+sound(y, Sf)
